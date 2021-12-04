5cb4ce28d628335805ec6d1bee5a912efb52c070
Arthur Kaukal Valladares <arthurkaukal@gmail.com>
2021-12-04 00:12:54 -0800

    Sketching out initial structs I will for sure need

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+pub struct Vec3 {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+pub struct Vec4 {
+    x: f32,
+    y: f32,
+    z: f32,
+    w: f32,
+}
+
+pub struct Mat3 {
+    n: [[f32; 3]; 3],
+}
+
+pub struct Mat4 {
+    n: [[f32; 4]; 4],
+}
+
+pub struct Quaternion {
+    x: f32,
+    y: f32,
+    z: f32,
+    w: f32,
+}
