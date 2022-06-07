d5ca76304e756097c00f8e2a949abd2fab5af5e2
ArthurValladares <arthurkaukal@gmail.com>
2022-06-07 00:09:49 -0700

    New Mat4 API

--- src/mat/mat4.rs
+++ src/mat/mat4.rs
@@ -1,24 +1,45 @@
-use crate::vec::Vec3;
+use crate::vec::{Vec3, Vec4};
 
 #[repr(C)]
 #[derive(Debug, Default, Copy, Clone, PartialEq)]
 #[rustfmt::skip]
 pub struct Mat4 {
-    data: [f32; 16],
+    data: [Vec4; 4],
 }
 
 impl Mat4 {
-    pub fn from_rows_array(d: [f32; 16]) -> Self {
-        Self { data: d }
+    pub fn from_data(
+        n00: f32,
+        n01: f32,
+        n02: f32,
+        n03: f32,
+        n10: f32,
+        n11: f32,
+        n12: f32,
+        n13: f32,
+        n20: f32,
+        n21: f32,
+        n22: f32,
+        n23: f32,
+        n30: f32,
+        n31: f32,
+        n32: f32,
+        n33: f32,
+    ) -> Self {
+        Self {
+            data: [
+                Vec4::new(n00, n10, n20, n30),
+                Vec4::new(n01, n11, n21, n31),
+                Vec4::new(n02, n12, n22, n32),
+                Vec4::new(n03, n13, n23, n33),
+            ],
+        }
     }
 
     pub fn identity() -> Mat4 {
-        Self::from_rows_array([
-            1.0, 0.0, 0.0, 0.0, //
-            0.0, 1.0, 0.0, 0.0, //
-            0.0, 0.0, 1.0, 0.0, //
-            0.0, 0.0, 0.0, 1.0,
-        ])
+        Self::from_data(
+            1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0,
+        )
     }
 
     pub fn rotate(t: f32, a: Vec3) -> Self {
@@ -34,26 +55,23 @@ impl Mat4 {
         let axaz = x * a.z();
         let ayaz = y * a.z();
 
-        Self::from_rows_array([
+        Self::from_data(
             c + x * a.x(),
-            axay + s * a.z(),
-            axaz - s * a.y(),
-            0.0,
-            //
-            axay - s * a.z(),
-            c + y * a.y(),
-            ayaz + s * a.x(),
-            0.0,
-            //
+            axay - s + a.z(),
             axaz + s * a.y(),
+            0.0,
+            axay + s * a.z(),
+            c + y * a.y(),
             ayaz - s * a.x(),
+            0.0,
+            axaz - s * a.y(),
+            ayaz + s * a.x(),
             c + z * a.z(),
             0.0,
-            //
             0.0,
             0.0,
             0.0,
             1.0,
-        ])
+        )
     }
 }
