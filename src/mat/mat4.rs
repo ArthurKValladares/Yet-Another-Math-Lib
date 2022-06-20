a34485b058fbaa9ead724b693a98148d6391a5ce
ArthurValladares <arthurkaukal@gmail.com>
2022-06-19 22:11:41 -0700

    Add more transform matrix functions

--- src/mat/mat4.rs
+++ src/mat/mat4.rs
@@ -1,4 +1,7 @@
-use crate::vec::{Vec3, Vec4};
+use crate::{
+    quat::Quat,
+    vec::{Vec3, Vec4},
+};
 
 #[repr(C)]
 #[derive(Debug, Default, Copy, Clone, PartialEq)]
@@ -47,7 +50,12 @@ impl Mat4 {
     }
 
     pub fn col(&self, idx: usize) -> Vec4 {
-        Vec4::new(self.data[0].idx(idx), self.data[1].idx(idx), self.data[2].idx(idx), self.data[3].idx(idx))
+        Vec4::new(
+            self.data[0].idx(idx),
+            self.data[1].idx(idx),
+            self.data[2].idx(idx),
+            self.data[3].idx(idx),
+        )
     }
 
     pub fn rotate_x(t: f32) -> Self {
@@ -109,6 +117,80 @@ impl Mat4 {
             1.0,
         )
     }
+
+    pub fn rotation_from_quat(quat: Quat) -> Self {
+        let x2 = quat.x() * quat.x();
+        let y2 = quat.y() * quat.y();
+        let z2 = quat.z() * quat.z();
+
+        let xy = quat.x() * quat.y();
+        let xz = quat.x() * quat.z();
+        let yz = quat.y() * quat.z();
+        let wx = quat.w() * quat.x();
+        let wy = quat.w() * quat.y();
+        let wz = quat.w() * quat.z();
+
+        Self::from_data(
+            1.0 - 2.0 * (y2 + z2),
+            2.0 * (xy - wz),
+            2.0 * (xz + wy),
+            0.0,
+            2.0 * (xy + wz),
+            1.0 - 2.0 * (x2 + z2),
+            2.0 * (yz - wx),
+            0.0,
+            2.0 * (xz - wy),
+            2.0 * (yz + wx),
+            1.0 - 2.0 * (x2 + y2),
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            1.0,
+        )
+    }
+
+    pub fn scale(vec: Vec3) -> Self {
+        Self::from_data(
+            vec.x(),
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            vec.y(),
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            vec.z(),
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            1.0,
+        )
+    }
+
+    pub fn translate(vec: Vec3) -> Self {
+        Self::from_data(
+            0.0,
+            0.0,
+            0.0,
+            vec.x(),
+            0.0,
+            0.0,
+            0.0,
+            vec.y(),
+            0.0,
+            0.0,
+            0.0,
+            vec.z(),
+            0.0,
+            0.0,
+            0.0,
+            1.0,
+        )
+    }
 }
 
 impl std::ops::Mul<Mat4> for Mat4 {
@@ -117,10 +199,43 @@ impl std::ops::Mul<Mat4> for Mat4 {
     fn mul(self, rhs: Mat4) -> Self::Output {
         Self {
             data: [
-                Vec4::new(self.data[0].dot(&rhs.col(0)), self.data[0].dot(&rhs.col(1)), self.data[0].dot(&rhs.col(2)), self.data[0].dot(&rhs.col(3))),
-                Vec4::new(self.data[1].dot(&rhs.col(0)), self.data[1].dot(&rhs.col(1)), self.data[1].dot(&rhs.col(2)), self.data[1].dot(&rhs.col(3))),
-                Vec4::new(self.data[2].dot(&rhs.col(0)), self.data[2].dot(&rhs.col(1)), self.data[2].dot(&rhs.col(2)), self.data[2].dot(&rhs.col(3))),
-                Vec4::new(self.data[3].dot(&rhs.col(0)), self.data[3].dot(&rhs.col(1)), self.data[3].dot(&rhs.col(2)), self.data[3].dot(&rhs.col(3))),
+                Vec4::new(
+                    self.data[0].dot(&rhs.col(0)),
+                    self.data[0].dot(&rhs.col(1)),
+                    self.data[0].dot(&rhs.col(2)),
+                    self.data[0].dot(&rhs.col(3)),
+                ),
+                Vec4::new(
+                    self.data[1].dot(&rhs.col(0)),
+                    self.data[1].dot(&rhs.col(1)),
+                    self.data[1].dot(&rhs.col(2)),
+                    self.data[1].dot(&rhs.col(3)),
+                ),
+                Vec4::new(
+                    self.data[2].dot(&rhs.col(0)),
+                    self.data[2].dot(&rhs.col(1)),
+                    self.data[2].dot(&rhs.col(2)),
+                    self.data[2].dot(&rhs.col(3)),
+                ),
+                Vec4::new(
+                    self.data[3].dot(&rhs.col(0)),
+                    self.data[3].dot(&rhs.col(1)),
+                    self.data[3].dot(&rhs.col(2)),
+                    self.data[3].dot(&rhs.col(3)),
+                ),
+            ],
+        }
+    }
+}
+
+impl From<[[f32; 4]; 4]> for Mat4 {
+    fn from(data: [[f32; 4]; 4]) -> Self {
+        Self {
+            data: [
+                data[0].into(),
+                data[1].into(),
+                data[2].into(),
+                data[3].into(),
             ],
         }
     }
--- src/quat/mod.rs
+++ src/quat/mod.rs
@@ -3,26 +3,49 @@ use crate::vec::Vec3;
 #[repr(C)]
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Quat {
-    x: f32,
-    y: f32,
-    z: f32,
-    w: f32,
+    data: [f32; 4],
 }
 
 impl Quat {
     pub fn from_axis_angle(axis: Vec3, angle: f32) -> Self {
         let s = (angle / 2.0).sin();
         Self {
-            x: axis.x() * s,
-            y: axis.y() * s,
-            z: axis.z() * s,
-            w: (angle / 2.0).cos(),
+            data: [
+                axis.x() * s,
+                axis.y() * s,
+                axis.z() * s,
+                (angle / 2.0).cos(),
+            ],
         }
     }
 
+    pub fn x(&self) -> f32 {
+        self.data[0]
+    }
+
+    pub fn y(&self) -> f32 {
+        self.data[1]
+    }
+
+    pub fn z(&self) -> f32 {
+        self.data[2]
+    }
+
+    pub fn w(&self) -> f32 {
+        self.data[3]
+    }
+
     pub fn rotate(&self, vec: Vec3) -> Vec3 {
-        let b = Vec3::new(self.x, self.y, self.z);
+        let b = Vec3::new(self.x(), self.y(), self.z());
         let b2 = b.magnitude_squared();
-        vec * (self.w * self.w - b2) + b * (vec.dot(&b) * 2.0) + b.cross(&vec) * (self.w * 2.0)
+        vec * (self.w() * self.w() - b2)
+            + b * (vec.dot(&b) * 2.0)
+            + b.cross(&vec) * (self.w() * 2.0)
+    }
+}
+
+impl From<[f32; 4]> for Quat {
+    fn from(data: [f32; 4]) -> Self {
+        Self { data }
     }
 }
