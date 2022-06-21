ca9c4b4ebb70848f01e0adb9e39cc577b596ded0
ArthurValladares <arthurkaukal@gmail.com>
2022-06-20 22:58:26 -0700

    From columns major function for Mat4

--- src/mat/mat4.rs
+++ src/mat/mat4.rs
@@ -41,6 +41,17 @@ impl Mat4 {
         }
     }
 
+    pub fn from_column_major(data: [[f32; 4]; 4]) -> Self {
+        Self {
+            data: [
+                Vec4::new(data[0][0], data[1][0], data[2][0], data[3][0]),
+                Vec4::new(data[0][1], data[1][1], data[2][1], data[3][1]),
+                Vec4::new(data[0][2], data[1][2], data[2][2], data[3][2]),
+                Vec4::new(data[0][3], data[1][3], data[2][3], data[3][3]),
+            ],
+        }
+    }
+
     pub fn identity() -> Mat4 {
         Self::from_data(
             1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0,
