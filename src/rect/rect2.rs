4a67565d3d420963a48af2e6d726cf560986fde9
ArthurValladares <arthurkaukal@gmail.com>
2022-10-13 23:00:25 -0700

    different types for rect offset and size

--- src/rect/rect2.rs
+++ src/rect/rect2.rs
@@ -2,24 +2,25 @@ use crate::{point::Point2D, size::Size2D};
 
 #[repr(C)]
 #[derive(Debug, Default, Copy, Clone, PartialEq)]
-pub struct Rect2D<N: num::Num + Copy> {
+pub struct Rect2D<N: num::Num + Copy, M: num::Num + Copy> {
     offset: Point2D<N>,
-    size: Size2D<N>,
+    size: Size2D<M>,
 }
 
-impl<N> Rect2D<N>
+impl<N, M> Rect2D<N, M>
 where
     N: num::Num + Copy,
+    M: num::Num + Copy,
 {
-    pub fn from_offset_and_size(offset: Point2D<N>, size: Size2D<N>) -> Self {
+    pub fn from_offset_and_size(offset: Point2D<N>, size: Size2D<M>) -> Self {
         Self { offset, size }
     }
 
-    pub fn from_size(size: Size2D<N>) -> Self {
+    pub fn from_size(size: Size2D<M>) -> Self {
         Self::from_offset_and_size(Point2D::zero(), size)
     }
 
-    pub fn from_width_height(width: N, height: N) -> Self {
+    pub fn from_width_height(width: M, height: M) -> Self {
         Self::from_size(Size2D::new(width, height))
     }
 
@@ -27,15 +28,15 @@ where
         self.offset
     }
 
-    pub fn size(&self) -> Size2D<N> {
+    pub fn size(&self) -> Size2D<M> {
         self.size
     }
 
-    pub fn width(&self) -> N {
+    pub fn width(&self) -> M {
         self.size.width()
     }
 
-    pub fn height(&self) -> N {
+    pub fn height(&self) -> M {
         self.size.height()
     }
 }
